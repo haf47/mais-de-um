09f219046e124e40f151988a1627954b9c43eb05
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-05 11:40:39 -0300

    Add files via upload

---
+++ 14-maisDeUmIf.js
@@ -0,0 +1,23 @@
+console.log ("Controle de idade para votar, seguindo normas do TRE.");
+
+const idadeLiberadaParaVotação = ('17 anos');
+
+console.log (idadeLiberadaParaVotação);
+
+console.log ("Não será permitido acesso ao local de votação, pessoas de bermudas ou trajes de banho.");
+
+
+
+if(idadeLiberadaParaVotação >=18) {
+
+    console.log ("Liberado para votar.");
+    
+} else {
+    console.log ("Não pode votar.")
+    
+}
+
+
+
+
+ 
